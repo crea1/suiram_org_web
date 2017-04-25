f395146e477731dfb5b54d1639bd623427585812
Marius Kristensen <marius.kristensen@gmail.com>
2017-04-25 20:53:17 +0200

    Started on welcome screen

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,17 +1,22 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+//const logo = require('../images/logo.svg');
+
+
 var Welcome = React.createClass({
-  render: function () {
-    return (
-      <div id="welcome-stuff">
-        <h1>Marius Kristensen</h1>
-      </div>
-    );
-  }
+    render: function () {
+        return (
+            <div id="welcome-stuff">
+                <div className="welcome-hi">Hi there, I'm</div>
+                <h1>Marius Kristensen</h1>
+                <div className="welcome-hi2">a developer living in Oslo, Norway</div>
+            </div>
+        );
+    }
 });
 
 ReactDOM.render(
-  <Welcome />,
-  document.getElementById('welcome')
+    <Welcome />,
+    document.getElementById('welcome')
 );
