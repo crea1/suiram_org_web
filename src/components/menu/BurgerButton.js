5d08aa857d0519bb3218dab5ce32a45e5413f76a
Marius Kristensen <marius.kristensen@gmail.com>
2017-10-17 20:08:10 +0200

    Add animation when clicking burger

--- src/components/menu/BurgerButton.js
+++ src/components/menu/BurgerButton.js
@@ -1,17 +1,36 @@
 import React from "react";
 
 export class BurgerButton extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {isActive: false};
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+
     render() {
         return (
-            <div className="menu-button">
+            <div className={"menu-button"}
+                 onClick={this.handleClick}>
                 <div className="burger-icon">
                     <div className="burger-container">
-                        <span className="burger-bun-top"/>
-                        <span className="burger-filling"/>
-                        <span className="burger-bun-bot"/>
+                        <span className={"burger-bun-top"  + (this.state.isActive ? " burger-clicked" :"")}/>
+                        <span className={"burger-filling"  + (this.state.isActive ? " burger-clicked" :"")}/>
+                        <span className={"burger-bun-bot"  + (this.state.isActive ? " burger-clicked" :"")}/>
                     </div>
                 </div>
             </div>
+        );
+    }
+
+    handleClick() {
+        this.setState(
+            prevState => ({
+                isActive: !prevState.isActive
+            })
         );
     }
 }
