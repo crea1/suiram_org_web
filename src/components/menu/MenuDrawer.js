8cd04237930d454c97d212f02540a43506dbb25e
Marius Kristensen <marius.kristensen@gmail.com>
2017-10-26 21:27:22 +0200

    Add MenuLink Component

--- src/components/menu/MenuDrawer.js
+++ src/components/menu/MenuDrawer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {MenuLink} from "./MenuLink";
 
 export class MenuDrawer extends React.Component {
 
@@ -6,8 +7,8 @@ export class MenuDrawer extends React.Component {
         return (
             <div className={"menu-drawer " + (this.props.drawerOpen ? "menu-open" : "")}>
                 <h1 style={{fontSize: '2vw'}}>menu</h1>
-                <div>Home</div>
-                <div>About</div>
+                <MenuLink linkname={"Home"} link={"#"}/>
+                <MenuLink linkname={"About"} link={"#about"}/>
             </div>
         );
     }
